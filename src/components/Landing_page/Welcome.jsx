5ca548cf5063c9e4c99f6364838c6f4a83972b0e
Eren Fil <efrieln@gmail.com>
2023-07-07 15:20:11 +0300

    Insert link glow

--- src/components/Landing_page/Welcome.jsx
+++ src/components/Landing_page/Welcome.jsx
@@ -310,9 +310,34 @@ export default function Welcome({ hasActiveSub, credit }) {
 							value={inputValue}
 							onChange={(event) => setInputValue(event.target.value)}
 							placeholder=" "
-							className="peer w-full border-t-blue-gray-500 h-full bg-white dark:bg-mildDarkMode text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 dark:place-holder-shown:border-t-darkMode placeholder-shown:border-t-blue-gray-200 border focus:border-2  focus:border-t-transparent dark:focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-500 dark:border-black dark:focus:border-r-green-400  dark:focus:border-l-green-400 dark:focus:border-b-green-400 focus:border-green-400"
+							className="peer w-full border-t-blue-gray-500 h-full bg-white dark:bg-mildDarkMode
+										text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0
+										enabled:hover:border-green-400 enabled:hover:border-2 enabled:hover:animate-pulse
+										enabled:focus:hover:border-t-transparent enabled:focus:hover:animate-none
+										disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border
+										placeholder-shown:border-blue-gray-200 dark:place-holder-shown:border-t-darkMode
+										placeholder-shown:border-t-blue-gray-200 border focus:border-2  focus:border-t-transparent
+										dark:focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-500
+										dark:border-black dark:focus:border-r-green-400  dark:focus:border-l-green-400
+										dark:focus:border-b-green-400 focus:border-green-400"
 						/>
-						<label class="text-zinc-400 flex w-full h-full select-none pointer-events-none absolute left-0 font-normal peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-blue-gray-400 peer-focus:text-green-400 before:border-blue-gray-200 dark:before:border-mildDarkMode dark:after:border-mildDarkMode peer-focus:before:!border-green-400 after:border-blue-gray-200 peer-focus:after:!border-green-400">
+						<label
+							class="text-zinc-400 flex w-full h-full select-none pointer-events-none absolute left-0
+										font-normal peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight
+										peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500
+										transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px]
+										before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px]
+										before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t
+										peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none
+										before:transition-all peer-disabled:before:border-transparent after:content[' ']
+										after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1
+										peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t
+										peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none
+										after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75]
+										text-blue-gray-400 peer-focus:text-green-400 before:border-blue-gray-200 dark:before:border-mildDarkMode
+										dark:after:border-mildDarkMode peer-focus:before:!border-green-400 after:border-blue-gray-200
+										peer-focus:after:!border-green-400"
+						>
 							Insert a link to start
 						</label>
 					</div>
@@ -346,7 +371,10 @@ export default function Welcome({ hasActiveSub, credit }) {
 						) : (
 							<div>
 								<a href="/u/login">
-									<Button className="bg-zinc-700 px-6 lg:px-10 py-3 dark:bg-green-400 dark:text-zinc-300 text-sm lg:text-[15px]  ">
+									<Button
+										className="bg-zinc-700 hover:bg-zinc-500 px-6 lg:px-10 py-3 dark:bg-green-400 dark:hover:bg-green-500
+														dark:text-zinc-300 text-sm lg:text-[15px] transition-colors ease-out"
+									>
 										Sign in to submit
 									</Button>
 								</a>
